31ff63e7676b9a8720426f5041ae69dea77c37cf
Frankgmv <giovanyvelasquez2004@gmail.com>
2023-06-13 13:21:27 -0500

    Creacion de archivos y productos

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+// TODO agregar fondo de nevera abierta 
+
+url = './assets/img';
+
+const bebidas = [
+    { id:1, nombre:'MrTea', descripcion:'pal almuerzo', path_url:'/colombiana.png', destacado:true},
+    { id:2, nombre:'Colombiana', descripcion:'sabe mejor que el put.. fizz', path_url:'/colombiana.png', destacado:false},
+    { id:3, nombre:'Coca Cola', descripcion:'La buena pa la diabetes pa', path_url:'/cocaCola.png', destacado:false},
+    { id:4, nombre:'Fanta', descripcion:'Pa la inalcanzable, descontinuada', path_url:'/fanta.png', destacado:false},
+    { id:5, nombre:'Cuatro', descripcion:'Pa la michelaita heladita', path_url:'/cuatro.png', destacado:false},
+    { id:6, nombre:'Manzana', descripcion:'Como esta no hay dos pa\'', path_url:'/manzana.png', destacado:false},
+    { id:7, nombre:'Hatsu', descripcion:'Sabe a basura pero buenísimo', path_url:'/hatsu.png', destacado:false},
+    { id:8, nombre:'Pepsi', descripcion:'Pa ahorrar un barrita en la Coca-Cola', path_url:'/pepsi.png', destacado:false},
+    { id:9, nombre:'Sprite', descripcion:'Pa hacer chirrinchi del fino', path_url:'/sprite.png', destacado:false},
+    { id:10, nombre:'Hit Mora', descripcion:'Pa combinar con la empanadita', path_url:'/hitmora.png', destacado:false},
+]
